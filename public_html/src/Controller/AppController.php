ebeb4d56b384d8d2fff159801d7937c868d66152
Git-admin <richardson-d@webmail.uwinnipeg.ca>
2015-03-15 02:06:09 -0500

    Fixing user beforeFilter() syntax.

--- public_html/src/Controller/AppController.php
+++ public_html/src/Controller/AppController.php
@@ -49,9 +49,11 @@ class AppController extends Controller
             ]
         ]);
 		
+
+    }
+	
 	public function beforeFilter(Event $event)
     {
         $this->Auth->allow(['index', 'view', 'display']);
-    }
     }
 }
