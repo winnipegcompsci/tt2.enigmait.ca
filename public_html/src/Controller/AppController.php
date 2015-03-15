cdac4b05ca68834cdbcd34b22b2b0668237c442b
Git-admin <richardson-d@webmail.uwinnipeg.ca>
2015-03-15 02:09:05 -0500

    Added reference to Cake\Event\Event.

--- public_html/src/Controller/AppController.php
+++ public_html/src/Controller/AppController.php
@@ -15,7 +15,7 @@
 namespace App\Controller;
 
 use Cake\Controller\Controller;
-
+use Cake\Event\Event;
 /**
  * Application Controller
  *
@@ -26,6 +26,11 @@ use Cake\Controller\Controller;
  */
 class AppController extends Controller
 {
+	public function beforeFilter(Event $event)
+    {
+        $this->Auth->allow(['index', 'view', 'display']);
+		// ...
+    }
 
     /**
      * Initialization hook method.
@@ -52,8 +57,5 @@ class AppController extends Controller
 
     }
 	
-	public function beforeFilter(Event $event)
-    {
-        $this->Auth->allow(['index', 'view', 'display']);
-    }
+
 }
