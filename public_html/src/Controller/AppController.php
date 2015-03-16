ea669e07ed3c2c20709edcf9490ce38ff579fd79
Git-admin <richardson-d@webmail.uwinnipeg.ca>
2015-03-16 17:35:52 -0500

    Removing cake warning.

--- public_html/src/Controller/AppController.php
+++ public_html/src/Controller/AppController.php
@@ -44,7 +44,7 @@ class AppController extends Controller
 	public function beforeFilter(Event $event)
     {
         $this->Auth->allow(['index', 'view', 'display']);
-		$this->set('logged_in', $this->Auth->loggedIn());
+		// $this->set('logged_in', $this->Auth->loggedIn());
 		// ...
     }
 
