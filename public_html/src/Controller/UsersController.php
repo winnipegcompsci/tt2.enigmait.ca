37e10eb708f5d3197769bcd84c06137b5217372d
Git-admin <richardson-d@webmail.uwinnipeg.ca>
2015-03-16 22:11:45 -0500

    Modified Users Controller beforeFilter.

--- public_html/src/Controller/UsersController.php
+++ public_html/src/Controller/UsersController.php
@@ -11,11 +11,14 @@ use App\Controller\AppController;
 class UsersController extends AppController
 {
 
-	public function beforeFilter(Event $event)
-	{
-		parent::beforeFilter($event);
-		$this->Auth->allow('add');
-	}	
+public function beforeFilter(Event $event)
+{
+    parent::beforeFilter($event);
+    // Allow users to register and logout.
+    // You should not add the "login" action to allow list. Doing so would
+    // cause problems with normal functioning of AuthComponent.
+    $this->Auth->allow(['add', 'logout']);
+}
 
 	public function login() 
 	{
