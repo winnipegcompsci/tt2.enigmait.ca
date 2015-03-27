fd137e3da5e52ce7e08178d3b241e3c36072a077
David Richardson <drichardson@enigmait.ca>
2015-03-27 11:21:50 -0500

    Testing $product_save without category ID.

--- public_html/src/Controller/WtcrVendorsController.php
+++ public_html/src/Controller/WtcrVendorsController.php
@@ -208,11 +208,11 @@ class WtcrVendorsController extends AppController
                     $product = $products->newEntity($this->request->data());
                     
                     $product->name = $description;
-                    $product->wtcr_vendor_id = 'EPROM';
+                    $product->wtcr_vendor_id = 1;
                     $product->vendor_sku = $supplier_sku;
-                    $product->wtcr_sku = 'WTCR-' . $supplier_sku;
+                    $product->wtcr_sku = 'WTCR-' . $supplier_sku;       // Create a VENDOR_SKU -> WTCR_SKU FUNC
                     $product->vendor_price = $supplier_price;
-                    $product->wtcr_category_id = $category;
+                    // $product->wtcr_category_id = $category;
                     $product->last_updated = date('Y-m-d H:i:s');
                     
                     // echo "<br />Product:: <pre>" . print_r($product, TRUE) . "</pre>";
