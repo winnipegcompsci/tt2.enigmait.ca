78ff968b8fdf0866873f793367cc7ad4fffcbe71
Git-admin <richardson-d@webmail.uwinnipeg.ca>
2015-03-30 18:50:22 -0500

    Checking $completion_options.

--- public_html/src/Controller/TicketsController.php
+++ public_html/src/Controller/TicketsController.php
@@ -43,6 +43,19 @@ class TicketsController extends AppController
         $this->set('ticket', $ticket);
         $this->set('_serialize', ['ticket']);
 		
+		$completion_options = array(
+			10 => '10%',
+			20 => '20%',
+			30 => '30%',
+			40 => '40%',
+			50 => '50%',
+			60 => '60%',
+			70 => '70%',
+			80 => '80%',
+			90 => '90%',
+			100 => '100%'
+		);
+		
 		$this->set('projects', $this->Tickets->Projects->find('list'));
 		$this->set('users', $this->Tickets->Users->find('list'));
 		$this->set('priorities', $this->Tickets->TicketPriorities->find('list'));
