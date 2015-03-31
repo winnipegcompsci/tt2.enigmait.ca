5f54a26017b988e78575b7a11692a8efef60276a
David Richardson <drichardson@enigmait.ca>
2015-03-31 09:55:39 -0500

    Testing $this->request.

--- public_html/src/Controller/TicketsController.php
+++ public_html/src/Controller/TicketsController.php
@@ -135,7 +135,7 @@ class TicketsController extends AppController
 	function update_ticket($id = null) {
 		$ticket = $this->Tickets->get($id);
 		
-		$this->Flash->success("<pre>" . print_r($this->request->params, TRUE) . "</pre>");
+		$this->Flash->success("<pre>" . print_r($this->request, TRUE) . "</pre>");
         
         return $this->redirect(['controller' => 'Tickets', 'action' => 'view', $id]);
 	}
