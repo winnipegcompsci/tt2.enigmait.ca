ba1945ccb32f37729a9c2248fc095705e362248c
Git-admin <richardson-d@webmail.uwinnipeg.ca>
2015-04-05 19:32:25 -0500

    Testing $wtcr_category_id = 0

--- public_html/src/Controller/WtcrVendorsController.php
+++ public_html/src/Controller/WtcrVendorsController.php
@@ -234,7 +234,7 @@ class WtcrVendorsController extends AppController
                         $product->wtcr_vendor_sku = $supplier_sku;
                         $product->mfg_part_num = $supplier_sku;       // Create a VENDOR_SKU -> WTCR_SKU FUNC
 						$product->vendor_price = $supplier_price;
-                        // $product->wtcr_category_id = $category;
+                        $product->wtcr_category_id = 0;
                         $product->last_updated = date('Y-m-d H:i:s');
                     } else { // $product exists. 
                         $product->product_name = $description;
@@ -242,7 +242,7 @@ class WtcrVendorsController extends AppController
                         $product->wtcr_vendor_sku = $supplier_sku;
                         $product->mfg_part_num = $supplier_sku;       // Create a VENDOR_SKU -> WTCR_SKU FUNC
                         $product->vendor_price = $supplier_price;
-                        // $product->wtcr_category_id = $category;
+                        $product->wtcr_category_id = 0;
                         $product->last_updated = date('Y-m-d H:i:s');
                     }
                     
