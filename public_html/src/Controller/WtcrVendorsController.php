e11218e119c1929719286652120626521a7cd5e0
Git-admin <richardson-d@webmail.uwinnipeg.ca>
2015-04-05 19:14:48 -0500

    Testing product data.

--- public_html/src/Controller/WtcrVendorsController.php
+++ public_html/src/Controller/WtcrVendorsController.php
@@ -229,11 +229,11 @@ class WtcrVendorsController extends AppController
                         $product = $products->newEntity();
 						error_log("Product::2:: " . print_r($product, TRUE));
 						
-                        $product->name = $description;
+                        $product->product_name = $description;
                         $product->wtcr_vendor_id = 0;
                         $product->wtcr_vendor_sku = $supplier_sku;
-                        $product->wtcr_sku = 'WTCR-' . $supplier_sku;       // Create a VENDOR_SKU -> WTCR_SKU FUNC
-                        $product->wvendor_price = $supplier_price;
+                        $product->mfg_part_num = $supplier_sku;       // Create a VENDOR_SKU -> WTCR_SKU FUNC
+						$product->vendor_price = $supplier_price;
                         // $product->wtcr_category_id = $category;
                         $product->last_updated = date('Y-m-d H:i:s');
                     } else {
