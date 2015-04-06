c809228cefcc2699443380cbe4577ed71de5d678
David Richardson <drichardson@enigmait.ca>
2015-04-06 12:27:57 -0500

    s/vendor_sku / wtcr_vendor_sku.

--- public_html/src/Controller/WtcrVendorProductsController.php
+++ public_html/src/Controller/WtcrVendorProductsController.php
@@ -67,7 +67,7 @@ class WtcrVendorProductsController extends AppController
     public function add_product($vendor_sku = null) {
         error_log('Vendor SKU:: ' . $vendor_sku);
         
-        $product = $this->WtcrVendorProducts->find('all')->where(['vendor_sku' => $vendor_sku]);
+        $product = $this->WtcrVendorProducts->find('all')->where(['wtcr_vendor_sku' => $vendor_sku]);
         $wtcrVendors = $this->WtcrVendorProducts->WtcrVendors->find('list', ['limit' => 200]);
         $wtcrCategories = $this->WtcrVendorProducts->WtcrProductCategories->find('list', ['limit' => 200]);
     
