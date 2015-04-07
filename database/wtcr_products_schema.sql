56b69c00df2c5a502cf7a96c01282fc955900f4c
David Richardson <drichardson@enigmait.ca>
2015-04-07 09:53:13 -0500

    Updating Schema with Product Name and Removing Vendor Details.

---
+++ database/wtcr_products_schema.sql
@@ -0,0 +1,68 @@
+CREATE DATABASE  IF NOT EXISTS `tt2` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `tt2`;
+-- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
+--
+-- Host: localhost    Database: tt2
+-- ------------------------------------------------------
+-- Server version	5.6.17-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `wtcr_products`
+--
+
+DROP TABLE IF EXISTS `wtcr_products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `wtcr_products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'A unique ID for the product',
+  `wtcr_vendor_id` int(11) NOT NULL COMMENT 'The Supplier that offers the product',
+  `wtcr_vendor_sku` varchar(40) NOT NULL COMMENT 'The SKU used by the supplier to identify the product',
+  `mfg_part_num` varchar(40) NOT NULL COMMENT 'Part # The Manufacturer Uses',
+  `wtcrsku` varchar(40) NOT NULL COMMENT 'The SKU used by WTCR to identify the product',
+  `wtcr_product_category_id` int(11) NOT NULL COMMENT 'The category the product falls in',
+  `wtcr_product_name` varchar(45) DEFAULT NULL,
+  `description` longtext COMMENT 'A description of the product',
+  `autoupdate` int(11) NOT NULL COMMENT '1 if Product Should Auto Update Price, 0 otherwise.',
+  `wtcr_vendor_price` float DEFAULT NULL COMMENT 'The Price Suggested based on Current Markup',
+  `suggestedmarkup` float DEFAULT '0.5' COMMENT 'The Suggested Markup Percentage for a Product',
+  `wtcrprice` float DEFAULT NULL COMMENT 'The Price WTCR Charges',
+  `wtcr_nid` varchar(10) DEFAULT NULL COMMENT 'The NID of the Product on WTCRs Website',
+  `lastupdated` datetime NOT NULL COMMENT 'The date/time this product was last updated from supplier',
+  `marketplace_data` longtext NOT NULL,
+  PRIMARY KEY (`id`,`wtcr_vendor_id`,`mfg_part_num`),
+  UNIQUE KEY `suppliersku` (`wtcr_vendor_sku`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `nid` (`wtcr_nid`)
+) ENGINE=InnoDB AUTO_INCREMENT=9695 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `wtcr_products`
+--
+
+LOCK TABLES `wtcr_products` WRITE;
+/*!40000 ALTER TABLE `wtcr_products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `wtcr_products` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-04-07  9:53:05
