cdb352fc4df075136753ae27006a6d7761eb20ad
David Richardson <drichardson@enigmait.ca>
2015-04-07 10:54:55 -0500

    Reimplementing add_vendor_product().

--- public_html/src/Controller/WtcrProductsController.php
+++ public_html/src/Controller/WtcrProductsController.php
@@ -62,6 +62,31 @@ class WtcrProductsController extends AppController
         $this->set('_serialize', ['wtcrProduct']);
     }
 
+    public function add_vendor_product($mfg_part_num)
+    {
+        $wtcrProduct = $this->WtcrProducts->newEntity();
+        if ($this->request->is('post')) {
+            $wtcrProduct = $this->WtcrProducts->patchEntity($wtcrProduct, $this->request->data);
+            if ($this->WtcrProducts->save($wtcrProduct)) {
+                $this->Flash->success('The wtcr product has been saved.');
+                return $this->redirect(['action' => 'index']);
+            } else {
+                $this->Flash->error('The wtcr product could not be saved. Please, try again.');
+            }
+        }
+        
+        $productVendors = TableRegistry::get('wtcr_vendor_products')->where(['mfg_part_num' => $mfg_part_num]);
+        echo "<pre>" . print_r($productVendors,  TRUE) . "</pre>";
+        
+        $wtcrVendors = $this->WtcrProducts->WtcrVendors->find('list', ['limit' => 200]);
+        $wtcrProductCategories = $this->WtcrProducts->WtcrProductCategories->find('list', ['limit' => 200]);
+        $this->set(compact('wtcrProduct', 'wtcrVendors', 'wtcrProductCategories'));
+        $this->set('_serialize', ['wtcrProduct']);
+        $this->set('this_product', $wtcrProduct);
+        $this->set('productVendors', $productVendors);
+    }
+    
+    
     /**
      * Edit method
      *
