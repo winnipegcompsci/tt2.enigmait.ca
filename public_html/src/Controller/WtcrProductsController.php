6128a2978abd343762466148b65288a44454ad4c
David Richardson <drichardson@enigmait.ca>
2015-04-07 10:49:06 -0500

    Testing wtcr_products_controller for vendors.

--- public_html/src/Controller/WtcrProductsController.php
+++ public_html/src/Controller/WtcrProductsController.php
@@ -66,8 +66,6 @@ class WtcrProductsController extends AppController
     
     public function add_vendor_product($mfg_part_num)
     {
-        die('Called add vendor product');
-        
         $wtcrProduct = $this->WtcrProducts->newEntity();
         if ($this->request->is('post')) {
             $wtcrProduct = $this->WtcrProducts->patchEntity($wtcrProduct, $this->request->data);
