e84fe28c1bf16f0d30ee9d958dab4ccf23e6b35b
David Richardson <drichardson@enigmait.ca>
2015-04-08 12:35:45 -0500

    Testing die(products).

--- public_html/src/Controller/WtcrProductsController.php
+++ public_html/src/Controller/WtcrProductsController.php
@@ -114,6 +114,7 @@ class WtcrProductsController extends AppController
         $this->set('categories', $wtcrProductCategories);
         $this->set('marketplaces', $marketplaces);
         
+        die(print_r("<pre>" . $this->wtcrProducts, TRUE) . "</pre>");
         // error_log( "DEBUG NEW PRODUCT:: <pre>" . print_r($wtcrProduct, TRUE) . "<pre>");
         return $this->redirect(['controller' => 'wtcr_vendors', 'action' => 'view_vendor_products', 'eprom']);
     }
