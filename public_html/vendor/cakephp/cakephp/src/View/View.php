61337726c63c84994dc9530737a43f272d67460f
David Richardson <drichardson@enigmait.ca>
2015-04-09 13:05:59 -0500

    Testing $this->block->close().

--- public_html/vendor/cakephp/cakephp/src/View/View.php
+++ public_html/vendor/cakephp/cakephp/src/View/View.php
@@ -790,7 +790,7 @@ class View
         $remainingBlocks = count($this->Blocks->unclosed());
 
         if ($initialBlocks !== $remainingBlocks) {
-            echo "THIS BLOCK START: " . $this->Blocks->start('file');
+            echo "THIS BLOCK START: " . $this->Blocks->close('file');
             
             throw new LogicException(sprintf(
                 'The "%s" block was left open. Blocks are not allowed to cross files.',
