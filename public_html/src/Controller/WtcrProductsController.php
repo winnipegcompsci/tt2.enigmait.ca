3210632ccd7fb8e90d9b97ca584974998aea4b36
David Richardson <drichardson@enigmait.ca>
2015-05-06 15:09:24 -0500

    Testing $this->URTL->build()/.

--- public_html/src/Controller/WtcrProductsController.php
+++ public_html/src/Controller/WtcrProductsController.php
@@ -85,11 +85,7 @@ class WtcrProductsController extends AppController
             $wtcrProduct->marketplace_data = serialize( array() );
             
             move_uploaded_file($_FILES['picture']['tmp_name'], WWW_ROOT . 'img' . DS . $_FILES['picture']['name']);
-            /*
-            copy($this->request->data['picture']['tmp_name'], 
-                WWW_ROOT . 'img' . DS . $this->request->data['picture']['name']
-            );
-            */           
+           
             $data = unserialize($wtcrProduct->pictures);
             $data[] = array(
                 'name' => $this->request->data['picture']['name'],
