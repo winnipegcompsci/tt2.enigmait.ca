0ea8dbb53fa4e179e0c6ab187460d5516eae18ed
David Richardson <drichardson@enigmait.ca>
2015-05-06 12:42:09 -0500

    Testing WTCR Products.

--- public_html/src/Controller/WtcrProductsController.php
+++ public_html/src/Controller/WtcrProductsController.php
@@ -71,9 +71,8 @@ class WtcrProductsController extends AppController
         $wtcrProduct = $this->WtcrProducts->newEntity();   
                                 
         if($this->request->is('post')) {                   
-            echo "::DEBUG:: <pre>" . print_r($this->request->data, TRUE) . "</pre>";
-            DIE('END OF PROCESSING');
-               
+            echo "<pre>" . print_r($this->request, TRUE) . "</pre>";
+            
             $wtcrProduct = $this->WtcrProducts->patchEntity($wtcrProduct, $this->request->data);
             debug($wtcrProduct);
             
