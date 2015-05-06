d825b3e890c94e860786ad319a07ccb2025b1933
David Richardson <drichardson@enigmait.ca>
2015-05-06 14:03:08 -0500

    Add_vendor_product() also using move_uploaded_file().

--- public_html/src/Controller/WtcrProductsController.php
+++ public_html/src/Controller/WtcrProductsController.php
@@ -84,14 +84,20 @@ class WtcrProductsController extends AppController
             $wtcrProduct->wtcr_nid = 0;
             $wtcrProduct->marketplace_data = serialize( array() );
             
+            move_uploaded_file($this->request->data['picture']['tmp_name'], 
+                WWW_ROOT . 'product_images' . DS . $this->request->data['picture']['name']
+            );
+            
+            
             $data = unserialize($wtcrProduct->pictures);
             $data[] = array(
                 'name' => $this->request->data['picture']['name'],
                 'type' => $this->request->data['picture']['type'],
-                'tmp_name' => $this->request->data['picture']['tmp_name'],
+                'path' => WWW_ROOT . 'product_images' . DS . $this->request->data['picture']['name'],                
                 'error' => $this->request->data['picture']['error'],
                 'size' => $this->request->data['picture']['size']
             );
+                                               
             $wtcrProduct->pictures = serialize($data);
             $wtcrProduct->wtcr_product_category = $this->request->data['wtcr_product_category_id'];    
             
